3936e108c954872824f9b84c37e2044126a8b88f
Articorse <articorse@abv.bg>
2019-05-10 16:19:22 +0100

    Fixing database connection again.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt-nodejs');
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
 });
