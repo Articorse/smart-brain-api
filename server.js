140a41a62d4b5c91e450fc0751083e6b2e7767e3
Articorse <articorse@abv.bg>
2019-05-10 16:09:44 +0100

    Updating database.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt-nodejs');
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-regular-93426',
       user : 'postgres',
       password : 'test',
       database : 'smart-brain'
@@ -26,7 +26,7 @@ app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
 app.put('/image', (res, req) => {image.handleImage(res, req, db)}); // Alternative Syntax
-app.post('/imageurl', image.handleApiCall); // Alternative Syntax
+app.post('/imageurl', image.handleApiCall);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on ${process.env.PORT}.`)
